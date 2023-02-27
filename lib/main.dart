73caaf16e708bb3a51904e342cb262854b695492
nafee <knafee@uco.edu>
2023-02-27 00:59:28 -0600

    adding comments

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:kwic_app/HomeScreen.dart';
+import 'package:kwic_app/UserInterface.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
